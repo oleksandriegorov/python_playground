063bdf7da5d11936185fe5fae40ffa336f826c13
Oleksandr Iegorov <alex.yegorov@gmail.com>
2020-09-22 21:35:17 +0300

    If any permutation of a string is a palindrome

---
+++ InterViewCake/AnyPermutationIsPalindrome/pp.py
@@ -0,0 +1,43 @@
+import unittest
+
+def has_palindrome_permutation(the_string):
+  letters = {}
+  for chr in the_string:
+    if chr in letters:
+      letters.pop(chr)
+    else:
+      letters[chr] = ''
+  # Check if any permutation of the input is a palindrome
+
+  return len(letters) <= 1
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcbcd')
+        self.assertTrue(result)
+
+    def test_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabccbdd')
+        self.assertTrue(result)
+
+    def test_no_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcd')
+        self.assertFalse(result)
+
+    def test_no_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabbcd')
+        self.assertFalse(result)
+
+    def test_empty_string(self):
+        result = has_palindrome_permutation('')
+        self.assertTrue(result)
+
+    def test_one_character_string(self):
+        result = has_palindrome_permutation('a')
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
